b6ac7c4ff121a2b871bf5f62627648c9fcfce7ba
Alan Foster <alan@alanfoster.me>
2017-12-25 23:30:21 +0000

    Add support for if / else, returns, as well as true/false

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -108,3 +108,54 @@ func TestAssignmentAndFunctions(t *testing.T) {
 		assert.Equal(t, expected.Literal, tok.Literal)
 	}
 }
+
+func TestBranching(t *testing.T) {
+	input := `
+		if ( 5 < 10 ) {
+			return true;
+		} else {
+			return false;
+		}
+	`
+
+	expectedTokens := []struct {
+		Type    token.TokenType
+		Literal string
+	}{
+		// if ( 5 < 10 ) {
+		{token.IF, "if"},
+		{token.LEFT_PAREN, "("},
+		{token.INT, "5"},
+		{token.LESS_THAN, "<"},
+		{token.INT, "10"},
+		{token.RIGHT_PAREN, ")"},
+		{token.LEFT_BRACE, "{"},
+
+		// return true;
+		{token.RETURN, "return"},
+		{token.TRUE, "true"},
+		{token.SEMICOLON, ";"},
+
+		// } else {
+		{token.RIGHT_BRACE, "}"},
+		{token.ELSE, "else"},
+		{token.LEFT_BRACE, "{"},
+
+		// return false;
+		{token.RETURN, "return"},
+		{token.FALSE, "false"},
+		{token.SEMICOLON, ";"},
+
+		// }
+		{token.RIGHT_BRACE, "}"},
+	}
+
+	l := New(input)
+
+	for _, expected := range expectedTokens {
+		tok := l.NextToken()
+
+		assert.Equal(t, expected.Type, tok.Type)
+		assert.Equal(t, expected.Literal, tok.Literal)
+	}
+}
